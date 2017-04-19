e3b2a8b16753376a16bc81ede93604dec058f5eb
azaleas <azaleas@2die4.com>
2017-04-19 18:13:21 +0400

    added question to admin

--- voting_app/polls/admin.py
+++ voting_app/polls/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Question, Choice
 
 # Register your models here.
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    raw_id_fields = ['question']
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ['author', 'question_text', 'created_date']
+    list_editable = ['question_text']
+    list_filter = ['created_date', 'updated_date']
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
--- voting_app/polls/models.py
+++ voting_app/polls/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Question(models.Model):
+    author = models.ForeignKey(User, related_name='questions')
+    question_text = models.CharField(max_length=300)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_date']
+
+    def __str__(self):
+        return self.question_text 
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, related_name='choices', on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    vote = models.PositiveIntegerField(db_index=True, default=0)
+
+    class Meta:
+        ordering = ['-vote']
+
+    def __str__(self):
+        return self.choice_text
