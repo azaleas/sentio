5ce24a3983522a32b3cb6fcfd2d0858382dba845
azaleas <azaleas@2die4.com>
2017-04-19 17:43:26 +0400

    added polls app

--- voting_app/voting_app/urls.py
+++ voting_app/voting_app/urls.py
@@ -19,6 +19,7 @@ from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^api/v1/', include('rest_framework.urls', namespace='rest_framework'))
 ]
 
 if settings.DEBUG:
