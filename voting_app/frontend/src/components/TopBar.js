8a0bed499fffab393404eb4e4e911ca4b7044fde
azaleas <azaleas@2die4.com>
2017-04-26 14:24:55 +0400

    received polls list - first view draft

--- voting_app/frontend/src/components/TopBar.js
+++ voting_app/frontend/src/components/TopBar.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
-import Link from 'react-router-dom/Link';
-
 const TopBar = () => (
-    <h2 className="ui green ribbon label app-label"><span>Sentio - a Voting App</span></h2>
+    <div className="topBar-wrapper">
+        <h2 className="ui green ribbon label app-label"><span>Sentio - a Voting App</span></h2>
+        <div className="ui divider"/>
+    </div>
 );
 
 export default TopBar;
--- voting_app/frontend/src/containers/PollsContainer.js
+++ voting_app/frontend/src/containers/PollsContainer.js
@@ -1,22 +1,67 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import api from './../utils/api';
 
 class PollsContainer extends Component {
     
     /*Container that will hold the list of all polls*/
 
-    static propTypes = {
-        className: PropTypes.string,
-    };
-
     constructor(props) {
         super(props);
+
+        this.state = {
+            fetched: false,
+            polls: [],
+        }
+    }
+
+    componentDidMount() {
+        this.getPolls();
+    }
+
+    getPolls() {
+        api.fetchAllPolls()
+            .then((polls) => {
+                this.setState({
+                    fetched: true,
+                    polls,
+                });
+            });
+    }
+
+    polls(){
+        console.log(this.state.polls);
+        return this.state.polls.map((poll) => {
+            return (
+                <div className="item">
+                    <div className="content">
+                        <div className="header">{poll.question_text}</div>
+                    </div>
+                </div>
+            )
+        });
     }
 
     render() {
         return (
-            <div className='ui active centered inline loader green' />
+            !this.state.fetched
+            ?(
+                <div className="ui active inverted dimmer">
+                    <div className="ui text loader">Loading the data...</div>
+                </div>
+            )
+            :(
+                <div className="ui relaxed divided list">
+                    {this.polls()}
+                </div>
+            )
         );
     }
 }
+
+PollsContainer.propTypes = {
+    className: PropTypes.string,
+};
 
 export default PollsContainer;
