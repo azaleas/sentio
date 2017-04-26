b1d4a57d6b04db986201f3441f3fac0897099fd1
azaleas <azaleas@2die4.com>
2017-04-26 22:05:31 +0400

    added single poll api method

--- voting_app/frontend/src/utils/api.js
+++ voting_app/frontend/src/utils/api.js
@@ -3,13 +3,28 @@ import axios from 'axios';
 const API_STEM = '/api/v1';
 
 let api = {
+
     fetchAllPolls: () => {
         let URL = `${API_STEM}/polls`;
         return axios.get(URL)
             .then((response) => response.data)
-            .catch(function(err){
-                console.warn("Error in fetchAllPolls", err);
-            })
+            .catch((error) => {
+                console.warn("Error in fetchAllPolls", error);
+            });
+    },
+
+    fetchSinglePoll: (id) => {
+        let URL = `${API_STEM}/polls/${id}`;
+        return axios.get(URL)
+            .then((response) => response.data)
+            .catch((error) => {
+                if (error.response.status === 404) {
+                    return error.response.status;
+                }
+                else{
+                    console.warn("Error in fetchAllPolls", error);
+                }
+            });
     }
 };
 
