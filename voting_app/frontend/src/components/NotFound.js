4653db6448147830911a2415b90944ae268f6075
azaleas <azaleas@2die4.com>
2017-04-26 22:04:05 +0400

    added 404 view

---
+++ voting_app/frontend/src/components/NotFound.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Link from 'react-router-dom/Link';
+
+const NotFound = ({ className }) => {
+    return (
+       <div className="ui negative message center">
+            <div className="header">404</div>
+            <p>Nothing was found here. Go to <Link to="/">main</Link>.</p>
+        </div> 
+    );
+};
+
+NotFound.propTypes = {
+    className: PropTypes.string,
+};
+
+export default NotFound;
