848907396db65826b6866fc246d48de8c44c919d
azaleas <azaleas@2die4.com>
2017-04-26 22:04:29 +0400

    added Polls component

---
+++ voting_app/frontend/src/components/Polls.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Link from 'react-router-dom/Link';
+
+const Polls = (props) => (
+    <div className="ui relaxed">
+        {
+            props.polls.map((poll, index) => (
+                <Link 
+                    to={`${props.path}/${poll.id}`}
+                    key={poll.id}
+                    >
+                    <div className="ui green segment poll-single">
+                            {poll.question_text}
+                    </div>
+                </Link>
+            ))
+        }
+    </div>
+);
+
+Polls.propTypes = {
+    className: PropTypes.string,
+};
+
+export default Polls;
