2efd3076527a00bd44095bb30eceda3228d668bb
azaleas <azaleas@2die4.com>
2017-05-02 21:44:33 +0400

    added first basic shallow test for App component. Also added setupTests file to fix localStorage error on tests

--- voting_app/frontend/src/tests/App.test.js
+++ voting_app/frontend/src/tests/App.test.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { shallow } from 'enzyme';
+import App from './../containers/App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-});
+describe('App', () => {
+    it('renders without crashing', () => {
+        shallow(<App />);
+    });
+})
