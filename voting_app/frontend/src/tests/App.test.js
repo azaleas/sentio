9356ee62afb290e05d8adb748446b6b9c6885547
azaleas <azaleas@2die4.com>
2017-05-03 17:35:50 +0400

    added tests for App component

--- voting_app/frontend/src/tests/App.test.js
+++ voting_app/frontend/src/tests/App.test.js
@@ -1,9 +1,91 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from './../containers/App';
+import {api} from './../utils/Api';
+
+jest.mock('./../utils/Api');
 
 describe('App', () => {
-    it('renders without crashing', () => {
-        shallow(<App />);
+    let wrapper;
+
+    beforeEach(() => {
+        wrapper = shallow(<App />);
     });
-})
+
+    afterEach(() => {
+        api.isLoggedIn.mockClear();
+        api.handleTwitterLogin.mockClear();
+    });
+    
+    it('renders without crashing', () => {
+        wrapper;
+    });
+
+    it('should have the `div` with class ".app-wrapper"', () => {
+        expect(
+            wrapper.find(".app-wrapper").exists()
+        ).toBe(true);
+    });
+
+    it('should have a `TopBar` element', () => {
+        expect(
+            wrapper.find('TopBar').exists()
+        ).toBe(true);
+    });
+
+    it('should have a `SideBar` element', () => {
+        expect(
+            wrapper.find('Sidebar').exists()
+        ).toBe(true);
+    });
+
+    describe('Sidebar', () => {
+        let sidebar;
+        beforeEach(() => {
+            sidebar = wrapper.find('Sidebar').first();
+            api.isLoggedIn.mockImplementation(() => true);
+            wrapper.setState({visible: true});
+            api.isLoggedIn();
+            wrapper.update();
+        });
+
+        it('should change state property `visible` to false \
+            if `hideMenu` is called', () => {
+            wrapper.instance().hideMenu();
+            expect(
+                wrapper.state().visible
+            ).toEqual(false);
+        });
+
+        it('should change state property `visible` to false \
+            if `hideMenuSegment` is called', () => {
+            wrapper.instance().hideMenuSegment();
+            expect(
+                wrapper.state().visible
+            ).toEqual(false);
+        });
+    });
+
+    describe('Login', () => {
+        beforeEach(() => {
+            api.handleTwitterLogin.mockImplementation(() => {
+                let response = new Promise((resolve, reject) =>{
+                    resolve('success');
+                });
+                return response;
+            });
+            wrapper.setState({fetchingToken: true});
+            wrapper.update();
+        });
+        it('should login when `twitterLoginStart` is triggered', () => {
+            wrapper.instance().twitterLoginStart();
+            return api.handleTwitterLogin()
+                .then((response) =>{
+                    expect(response).toEqual('success');
+                    expect(
+                        wrapper.state().fetchingToken
+                    ).toEqual(false);
+                });
+        })
+    });
+})  
