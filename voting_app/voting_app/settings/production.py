b8e094e3a65126307b36a557f1464079e8c2f0ec
azaleas <azaleas@2die4.com>
2017-05-06 16:14:52 +0400

    changed db settings and added heroku env variable

--- voting_app/voting_app/settings/production.py
+++ voting_app/voting_app/settings/production.py
@@ -22,9 +22,11 @@ SERVER_EMAIL = os.environ.get('EMAIL_USER')
 
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
-
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
+}
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
