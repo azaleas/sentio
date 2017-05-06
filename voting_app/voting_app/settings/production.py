15118a45442eb948b4190b347aaebd55bde4e5f6
azaleas <azaleas@2die4.com>
2017-05-06 21:49:55 +0400

    debug to true to check the errors

--- voting_app/voting_app/settings/production.py
+++ voting_app/voting_app/settings/production.py
@@ -1,7 +1,7 @@
 from .base import *
  
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 TEMPLATE_DEBUG = False
  
 # CHANGE THE ALLOWED_HOSTS LIST TO FIT YOUR NEEDS
