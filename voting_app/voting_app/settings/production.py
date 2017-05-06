80bf35213a255ce5451d3a5d7d3530c7b4fc2caf
azaleas <azaleas@2die4.com>
2017-05-06 19:38:32 +0400

    changed production debug to false, to see the errors

--- voting_app/voting_app/settings/production.py
+++ voting_app/voting_app/settings/production.py
@@ -1,7 +1,7 @@
 from .base import *
  
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = False
  
 # CHANGE THE ALLOWED_HOSTS LIST TO FIT YOUR NEEDS
