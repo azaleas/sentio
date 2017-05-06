5cfd30d156603b934f55c25f584e8b9c5c7d4f95
azaleas <azaleas@2die4.com>
2017-05-06 18:48:06 +0400

    modified wsgi.py for testing

--- voting_app/voting_app/wsgi.py
+++ voting_app/voting_app/wsgi.py
@@ -12,7 +12,8 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "voting_app.settings")
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "voting_app.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "voting_app.settings.production")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
