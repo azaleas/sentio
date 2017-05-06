20e570a48540a1f812c40d601625af6ce604bba8
azaleas <azaleas@2die4.com>
2017-05-06 19:47:23 +0400

    changed production debug to false, to see the errors

--- voting_app/voting_app/settings/production.py
+++ voting_app/voting_app/settings/production.py
@@ -1,11 +1,11 @@
 from .base import *
  
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = False
  
 # CHANGE THE ALLOWED_HOSTS LIST TO FIT YOUR NEEDS
-ALLOWED_HOSTS = ['.sentioapp.herokuapp.com']
+ALLOWED_HOSTS = ['.sentioapp.herokuapp.com', ]
 
 ADMINS = [('azaleas', 'azaleas@2die4.com')]
 
