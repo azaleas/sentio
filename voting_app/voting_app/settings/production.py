9ccb59697a22cd9a6acf6225a22ec960f3853e6b
azaleas <azaleas@2die4.com>
2017-05-28 20:03:18 +0400

    enable browsable api

--- voting_app/voting_app/settings/production.py
+++ voting_app/voting_app/settings/production.py
@@ -39,11 +39,11 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # Disable browsable API
 
-REST_FRAMEWORK = {
-    'DEFAULT_RENDERER_CLASSES': (
-        'rest_framework.renderers.JSONRenderer',
-    )
-}
+#REST_FRAMEWORK = {
+#    'DEFAULT_RENDERER_CLASSES': (
+#        'rest_framework.renderers.JSONRenderer',
+#   )
+#}
 
 # LOGGING
 LOGGING = {
